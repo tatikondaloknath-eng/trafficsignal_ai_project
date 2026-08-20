a73f425bb6db892c4cc2dfcc774e2df31f16767f
tatikondaloknath-eng <tatikondaloknath@gmail.com>
2026-08-20 21:47:04 +0530

    Change app to run on dynamic PORT
    Update app to use dynamic port from environment variable.

--- app.py
+++ app.py
@@ -356,4 +356,6 @@ def handle_settings():
     return jsonify(SYSTEM_SETTINGS)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    # Render assigns a dynamic PORT. Default to 5000 for local testing.
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
